33a556927a86b27d2771f478fe783c020b116055
Ben White <bwhite2140@outlook.com>
2020-06-12 09:06:47 -0700

    add input form to html and user interface logic to javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
+  $("form#list").submit(function(event) {
+    event.preventDefault();
 
+    const number = $("input#number").val();
+    const replacedNumbers = mrRogersReplacer(number);
+
+    $("#output").text(replacedNumbers);
+  });
 });
