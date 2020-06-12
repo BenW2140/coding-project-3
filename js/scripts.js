dcb1149eb6ba96499e82fd78dff55766a5c5156a
Ben White <bwhite2140@outlook.com>
2020-06-12 10:13:18 -0700

    add business logic for spec 4

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,20 @@
 const mrRogersReplacer = function (num) {
   const numbers = [];
   for (let i = 0; i <= num; i++) {
-    numbers.push('' + i + '');
+    numbers.push(' ' + i + '');
     // for (let j = 0; j < numbers[i].length; j++) {
     //   slicedNum = numbers[i].slice(j, j + 1);
     //   if (slicedNum.includes('1')) {
     //     numbers[i] = numbers[i].replace(numbers[i], 'Beep!');
     //   }
     // }
-    if (numbers[i].includes('2')) {
-      numbers[i] = numbers[i].replace(numbers[i], 'Boop!');
+    if (numbers[i].includes('3')) {
+      numbers[i] = numbers[i].replace(numbers[i], " Won't you be my neighbor?");
+    } else if (numbers[i].includes('2')) {
+      numbers[i] = numbers[i].replace(numbers[i], ' Boop!');
     } else if (numbers[i].includes('1')) {
-      numbers[i] = numbers[i].replace(numbers[i], 'Beep!');
+      numbers[i] = numbers[i].replace(numbers[i], ' Beep!');
     }
-    console.log(numbers);
   }
   return numbers;
 }
